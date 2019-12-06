ddab69c8bcf9b7fdf57a60b5af0dd3d046199069
Dallas Gollogly <dgollogly13@gmail.com>
2019-12-06 12:48:51 -0700

    file update

--- UFO_level_1/app.js
+++ UFO_level_1/app.js
@@ -1,4 +1,28 @@
 // from data.js
-var tableData = data;
+var ufoData = data;
+var table = d3.select("tbody");
 
-// YOUR CODE HERE!
+function buildTable (tableData) {
+    table.html ("");
+    tableData.forEach((tableRow) => {
+        var row = table.append("tr")
+        Object.values(tableRow).forEach((value) => {
+            var dataCell = row.append("td")
+            dataCell.text(value)
+        })
+    })
+}
+
+buildTable(ufoData);
+
+var button = d3.select("#filter-btn");
+
+button.on('click', function() {
+    d3.event.preventDefault;
+    var inputValue = d3.select('#datetime').property('value');
+    var ufoDate = ufoData.filter(function(ufo) {
+        return ufo.datetime === inputValue;
+    });
+    buildTable(ufoDate)
+});
+
